ddc57c952467dcc62c4616ba09c02f102c387194
= <sohailahmed34280@gmail.com>
2024-08-07 12:17:52 -0700

    collect staticfiles for production

---
+++ staticfiles/myapp_portfolio/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from myapp_portfolio.models import Certifications, Projects
+
+admin.site.register(Certifications)
+admin.site.register(Projects)
--- staticfiles/portfolio/settings.py
+++ staticfiles/portfolio/settings.py
@@ -62,13 +62,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Default DB
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
