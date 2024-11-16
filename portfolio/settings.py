b52c5edaa4187bdbe1cef253791f6d6a4d2cbb30
= <sohailahmed34280@gmail.com>
2024-11-16 22:56:58 +0500

    server media file on cloudinary

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Settings for Media
 MEDIA_URL = '/media/'
