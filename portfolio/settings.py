f3e1b72a2ec3f760937459a0527828ee5f041bb1
= <sohailahmed34280@gmail.com>
2024-11-16 22:45:03 +0500

    server media file on cloudinary

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%2*h686*+bvoiq-jboli=jn@qv0lkec!gy=ar66$vv7z#hm4lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
