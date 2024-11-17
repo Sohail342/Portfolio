f6eb0426c29cfcfb07326a1a8790d2cf499fe98b
= <sohailahmed34280@gmail.com>
2024-11-17 12:55:52 +0500

    Debug=False

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%2*h686*+bvoiq-jboli=jn@qv0lkec!gy=ar66$vv7z#hm4lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
